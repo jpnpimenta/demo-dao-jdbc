db557ead7d1e0397ebf1356c7ed962ac795d7627
Jose Pimenta <jpnpimenta@gmail.com>
2019-05-26 18:13:22 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,15 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, dep );
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id : " + newSeller.getId());
+
+		System.out.println("===== Test: 5 Seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
+
+	
+	
 	}
 
 }
