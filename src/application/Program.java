0c20295fde1770c0379298058b39bbd07b118d5a
Jose Pimenta <jpnpimenta@gmail.com>
2019-05-26 16:41:46 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,7 +32,10 @@ public class Program {
 			System.out.println(obj);
 		}
 	
-
+		System.out.println("===== Test: 4 Seller Insertion =====");
+		Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.0, dep );
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id : " + newSeller.getId());
 	}
 
 }
